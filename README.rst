842c24a5e674052faf80c6e832a584817ea3d45b
Vineet <naikvin@gmail.com>
2013-06-13 23:11:37 +0530

    Initial commit

---
+++ README.rst
@@ -0,0 +1,23 @@
+Pyharbor
+========
+
+What?
+-----
+
+A library for working with har (HTTP Archive) files
+
+
+Why one more har file lib?
+--------------------------
+
+1. I wanted to extract data out of har files and none of the existing
+   libs I found were well documented.
+2. Using Python 3 which I really wanted to get started with.
+3. To experiment writing stratified functional abstractions in Python.
+
+
+Status
+------
+
+Just started with it. No docs. No tests. You decide!
+
---
+++ pyharbor/pyharbor.py
@@ -0,0 +1,76 @@
+import json
+
+
+filename = '/home/vineet/Dropbox/kodecrm.com404.har'
+
+
+class Har(object):
+
+    def __init__(self, filename):
+        self.filaname = filename
+        self.har = read_har(filename)
+
+    def entries(self, include=None, exclude=None, **kwargs):
+        entries = filter_entries(self.har, **kwargs)
+        if include is not None:
+            return include_keys(entries, include)
+        elif exclude is not None:
+            return exclude_keys(entries, exclude)
+        else:
+            return entries
+
+
+def read_har(filename):
+    with open(filename, 'r') as f:
+        return json.load(f)
+
+
+def filter_entries(har, **kwargs):
+    pred = kwargs.pop('pred', None)
+    if pred is None:
+        pred = lambda e: all(get_key(e, k) == v for k, v in kwargs.items())
+    return (e for e in har['log']['entries'] if pred(e))
+
+
+def include_keys(entries, fields):
+    return (dict((f, get_key(e, f)) for f in fields) for e in entries)
+
+
+def exclude_keys(entries, fields):
+    raise NotImplementedError
+
+
+def get_key(_dict, key):
+    parts = key.split('__', 1)
+    try:
+        result = _dict[parts[0]]
+    except KeyError:
+        return None
+    else:
+        return result if len(parts) == 1 else get_key(result, parts[1])
+
+
+def test():
+    d = dict([('a', 'A'),
+              ('p', {'q': 'Q'}),
+              ('x', {'y': {'z': 'Z'}})])
+    assert get_key(d, 'a') == 'A'
+    assert get_key(d, 'p__q') == 'Q'
+    assert get_key(d, 'x__y__z') == 'Z'
+    print('ok. All tests pass.')
+
+
+if __name__ == '__main__':
+    test()
+
+    # Usage:
+    #
+    ## high level object oriented abstraction
+    # har = Har(filename)
+    # entries = har.entries(response__status=404, include=['request__url', 'response__status'])
+    # print(list(entries))
+
+    ## lower level functional abstraction
+    # har = read_har(filename)
+    # entries = filter_entries(har, response__status=404)
+    # print(list(include_keys(entries, ['request__url', 'response__status'])))
