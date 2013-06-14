2c37e4021f32c07557a152603939b498c685afff
Vineet <naikvin@gmail.com>
2013-06-15 00:20:24 +0530

    Add get_entries function
    filter_entries will use this to get the entries instead of accessing it
    directly

--- pyharbor/pyharbor.py
+++ pyharbor/pyharbor.py
@@ -8,7 +8,7 @@ class Har(object):
         self.har = read_har(filename)
 
     def entries(self, include=None, exclude=None, **kwargs):
-        entries = filter_entries(self.har, **kwargs)
+        entries = filter_entries(get_entries(self.har), **kwargs)
         if include is not None:
             return include_keys(entries, include)
         elif exclude is not None:
@@ -17,16 +17,26 @@ class Har(object):
             return entries
 
 
+class HarError(Exception):
+    pass
+
+
 def read_har(filename):
     with open(filename, 'r') as f:
         return json.load(f)
 
 
-def filter_entries(har, **kwargs):
-    pred = kwargs.pop('pred', None)
+def get_entries(har):
+    try:
+        return har['log']['entries']
+    except KeyError as e:
+        raise HarError('Har file format invalid. Key {e} not found'.format(e=e))
+
+
+def filter_entries(entries, pred=None, **kwargs):
     if pred is None:
         pred = lambda e: all(get_key(e, k) == v for k, v in kwargs.items())
-    return (e for e in har['log']['entries'] if pred(e))
+    return (e for e in entries if pred(e))
 
 
 def include_keys(entries, fields):
@@ -77,6 +87,6 @@ if __name__ == '__main__':
     ## lower level functional abstraction
     #
     # har = read_har(filename)
-    # entries = filter_entries(har, response__status=404)
+    # entries = filter_entries(get_entries(har), response__status=404)
     # print(list(include_keys(entries, ['request__url', 'response__status'])))
 
