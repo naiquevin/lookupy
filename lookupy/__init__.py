8b4a3741e8e88ae854cc7c075f752bc7238d0875
Vineet <naikvin@gmail.com>
2013-06-29 20:23:17 +0530

    Allow 'Collection' & 'Q' to be imported from pkg

--- lookupy/__init__.py
+++ lookupy/__init__.py
@@ -1,1 +1,13 @@
+"""
+   lookupy
+   ~~~~~~~
+
+   Lookupy is a Python library that provides a Django QuerySet like
+   interface to query (select and filter) data (list of dicts)
+
+"""
+
+from .lookupy import Collection, Q
+
+__all__ = ["Collection", "Q"]
 
