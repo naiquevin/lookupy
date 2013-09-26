ce606a9dfce7506da0072832cb5ea7c96163d582
Vineet <naikvin@gmail.com>
2013-09-26 23:01:16 +0530

    Handle building the package in absence of README.md

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from distutils.core import setup
 
+try:
+    long_desc = open('./README.md').read()
+except IOError:
+    long_desc = 'See: https://github.com/naiquevin/lookupy/blob/master/README.md'
+
 setup(
     name='Lookupy',
     version='0.1',
@@ -8,6 +13,6 @@ setup(
     packages=['lookupy',],
     license='MIT License',
     description='Django QuerySet inspired interface to query list of dicts',
-    long_description=open('README.md').read(),
+    long_description=long_desc,
 )
 
