2489c14e358d44d89e2db6fafd5bb2928fc9e1fc
Vineet <naikvin@gmail.com>
2013-09-28 19:00:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from distutils.core import setup
 
 try:
-    long_desc = open('./README.md').read()
+    long_desc = open('./README.rst').read()
 except IOError:
-    long_desc = 'See: https://github.com/naiquevin/lookupy/blob/master/README.md'
+    long_desc = 'See: https://github.com/naiquevin/lookupy/blob/master/README.rst'
 
 setup(
     name='Lookupy',
     version='0.1',
     author='Vineet Naik',
     author_email='naikvin@gmail.com',
+    url='https://github.com/naiquevin/lookupy',
     packages=['lookupy',],
     license='MIT License',
     description='Django QuerySet inspired interface to query list of dicts',
